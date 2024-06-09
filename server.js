b7121d5b3e2eb39909b9e4e3a4bcef30af02d668
“Fabio15vieira” <a18615@alunos.ipca.pt>
2024-06-09 22:43:38 +0100

    login e registo

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 
+
 const app = express();
 const db = new sqlite3.Database('./database/database.db');
 
@@ -12,17 +13,15 @@ app.use(bodyParser.json());
 
 app.get('/search', (req, res) => {
     const query = req.query.query;
-    db.all(
-        `SELECT * FROM Museu WHERE nome LIKE ?`,
-        [`%${query}%`],
-        (err, rows) => {
-            if (err) {
-                res.status(500).json({ message: 'error', error: err.message });
-            } else {
-                res.json({ message: 'success', data: rows });
-            }
+    const sql = "SELECT * FROM Museu WHERE nome LIKE ?";
+    const parametros = [`%${query}%`];
+    db.all(sql, parametros, (err, rows) => {
+        if (err) {
+            res.status(500).json({ message: 'error', error: err.message });
+        } else {
+            res.json({ message: 'success', data: rows });
         }
-    );
+    });
 });
 
 // Endpoint para registrar um novo usuário
